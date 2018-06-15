cd11d64975437cce6255017d19ece0a21a7b5fe5
Krystyna Szurmańska <kszurmanska@gmail.com>
2018-06-15 13:56:30 +0200

    Update the slider

--- js/index.js
+++ js/index.js
@@ -1,12 +1,26 @@
-var elem = document.querySelector('.main-carousel');
+var elem = document.querySelector('.carousel');
 var flkty = new Flickity( elem, {
   // options
   cellAlign: 'left',
-  contain: true
+  contain: true,
+  pageDots: false,
+  hash: true
 });
 
 // element argument can be a selector string
 //   for an individual element
-var flkty = new Flickity( '.main-carousel', {
-  // options
+var flkty = new Flickity( '.carousel', {
+  
+});
+
+var restart = document.getElementById('restart');
+restart.addEventListener( 'click', function() {
+  flkty.selectCell("#carousel-cell1");
+});
+
+var progressBar = document.querySelector('.progress-bar')
+
+flkty.on( 'scroll', function( progress ) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
 });
